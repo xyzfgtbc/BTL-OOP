81046a58e4540d466efb95a811958154160d17cf
Minhle2905 <xyzfgtbc@gmail.com>
2025-11-28 23:30:54 +0700

    Update Employee.java

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,1 +1,25 @@
+package com.cstore.model;
 
+/** Lop Employee - Quan ly thong tin nhan vien. */
+public class Employee {
+    private String id;
+    private String name;
+    private String position;
+    private double salary;
+
+    public Employee(String id, String name, String position, double salary) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public Object[] toRowData() {
+        return new Object[]{id, name, position, String.format("%,.0f", salary)};
+    }
+    
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getPosition() { return position; }
+    public double getSalary() { return salary; }
+}
