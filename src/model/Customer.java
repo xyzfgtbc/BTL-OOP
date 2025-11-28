a8a0138d778e4f1200bb39c42797337f902dbb0d
Minhle2905 <xyzfgtbc@gmail.com>
2025-11-28 23:33:16 +0700

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,1 +1,34 @@
+package com.cstore.model;
 
+/** Lop Customer - Quan ly thong tin va diem tich luy cua khach hang. */
+public class Customer {
+    private int id;
+    private String name;
+    private String phoneNumber;
+    private int loyaltyPoints;
+
+    public Customer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.loyaltyPoints = 0;
+    }
+
+    public Customer(int id, String name, String phoneNumber, int loyaltyPoints) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.loyaltyPoints = loyaltyPoints;
+    }
+
+    public Object[] toRowData() {
+        return new Object[]{id, name, phoneNumber, loyaltyPoints};
+    }
+    
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public int getLoyaltyPoints() { return loyaltyPoints; }
+    public void setLoyaltyPoints(int loyaltyPoints) { this.loyaltyPoints = loyaltyPoints; }
+    public void setName(String name) { this.name = name; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+}
