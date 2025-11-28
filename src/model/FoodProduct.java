bf6e2e650d5d75c4c2f33cda341392a934841f69
Minhle2905 <xyzfgtbc@gmail.com>
2025-11-28 23:29:11 +0700

    Update FoodProduct.java

--- src/model/FoodProduct.java
+++ src/model/FoodProduct.java
@@ -1,1 +1,22 @@
+package com.cstore.model;
 
+/** Lop FoodProduct - Ke thua tu Product. */
+public class FoodProduct extends Product {
+    private String expiryDate;
+
+    public FoodProduct(String id, String name, double basePrice, int quantity, String expiryDate) {
+        super(id, name, basePrice, quantity);
+        this.expiryDate = expiryDate;
+    }
+
+    @Override
+    public double calculatePrice() {
+        return getBasePrice() * 1.05; // 5% VAT
+    }
+
+    @Override
+    public String getTypeName() { return "Do An"; }
+    
+    @Override
+    public String getExtraInfo() { return expiryDate; }
+}
